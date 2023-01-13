8a78cfe20a49996b651c807b8af62b658caddfad
Захаров Павел <pzakharov83@gmail.com>
2023-01-13 16:51:15 +0300

    add new dependencies

--- voucher/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('contragent/<int:company>', views.voucher_index, name='voucher'),
-    path('voucher/<int:voucher>', views.codes_index, name='codes'),
-]
