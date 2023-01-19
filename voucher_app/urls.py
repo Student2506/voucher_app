c817cc3b5acc854a0078eeb6c09494316a4c667f
Захаров Павел <pzakharov83@gmail.com>
2023-01-19 09:27:31 +0300

    new app

--- voucher_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("oauth2/", include('django_auth_adfs.urls')),
-]
