ff725dc4e1cda2141b341c13d20e62f31d3499a6
Alexandr <102781985+Eshenok@users.noreply.github.com>
2023-03-05 23:09:22 +0600

    clear order/tepmplates update

--- frontend/src/components/Main/Vouchers/Vouchers.js
+++ frontend/src/components/Main/Vouchers/Vouchers.js
@@ -1,25 +1,39 @@
 import Customers from "./Customers/Customers";
 import Templates from "./Templates/Templates";
 import Orders from "./Orders/Orders";
+import { useSelector } from "react-redux";
 
 export default function Vouchers() {
+
+  const {templates, orders} = useSelector(state => state.customers);
 
   return (
     <section className="vouchers">
       <Customers />
       <div className="vouchers__main">
-        <article className="vouchers__orders vouchers__block">
-          <h2 className="vouchers__title">Заказы</h2>
-          <div className="vocuhers__block-container">
-            <Orders />
-          </div>
-        </article>
-        <article className="vouchers__templates vouchers__block">
-          <h2 className="vouchers__title">Шаблоны</h2>
-          <div className="vocuhers__block-container">
-            <Templates />
-          </div>
-        </article>
+        {
+          orders.length > 0 ?
+            <>
+              <article className="vouchers__orders vouchers__block">
+                <h2 className="vouchers__title">Заказы</h2>
+                <div className="vocuhers__block-container">
+                  <Orders />
+                </div>
+              </article>
+              {
+                templates.length > 0 ?
+                  <article className="vouchers__templates vouchers__block">
+                    <h2 className="vouchers__title">Шаблоны</h2>
+                    <div className="vocuhers__block-container">
+                      <Templates />
+                    </div>
+                  </article>
+                  : <></>
+              }
+            </>
+            : <div className="voucher__nothing"></div>
+        }
+
       </div>
     </section>
   )
