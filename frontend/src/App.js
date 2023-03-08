0b9b97eb0bd5f0c89c61834b4a298b8d9ee94af7
Alexandr <102781985+Eshenok@users.noreply.github.com>
2023-03-08 17:29:21 +0600

    cookie check

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,6 +34,14 @@ function App() {
     }
   }, [loggedIn])
 
+  function get_cookie(name){
+    return document.cookie.split(';').some(c => {
+      return c.trim().startsWith(name + '=');
+    });
+  }
+
+  console.log(get_cookie());
+
   return (
     <>
       <Switch>
