8677fb6429baeffa067d1efc23e24d104e6efa48
Alexandr <102781985+Eshenok@users.noreply.github.com>
2023-03-09 19:37:18 +0600

    logout v.02

--- frontend/src/components/popups/MenuPopup/MenuPopup.js
+++ frontend/src/components/popups/MenuPopup/MenuPopup.js
@@ -8,17 +8,17 @@ export function MenuPopup({ isOpen, onClose, login }) {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  // function exit() {
-  //   dispatch(exitUser());
-  //   history.push('/sign-in');
-  // }
+  function exit() {
+    dispatch(exitUser());
+    history.push('/sign-in');
+  }
 
   return(
     <article className={`menu ${isOpen ? "menu_open" : ""}`}>
       <div className="menu__head">
         <p className="menu__name">{ login }</p>
       </div>
-      <button className="button button_theme_blue" style={{margin: 0}}><a href={'https://adfs.karo-film.ru/adfs/oauth2/logout'}>Выйти</a></button>
+      <button className="button button_theme_blue" style={{margin: 0}} onClick={exit}><a href={'https://adfs.karo-film.ru/adfs/oauth2/logout'}>Выйти</a></button>
       <button className="button button_icon_close button_place_menu" onClick={() => {onClose()}} />
     </article>
   )
