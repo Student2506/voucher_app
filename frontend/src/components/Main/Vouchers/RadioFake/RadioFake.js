6b63a8ec15fe219d9e24555320c6c2e8f221749f
Alexandr <102781985+Eshenok@users.noreply.github.com>
2023-03-10 23:34:34 +0600

    checked items

--- frontend/src/components/Main/Vouchers/RadioFake/RadioFake.js
+++ frontend/src/components/Main/Vouchers/RadioFake/RadioFake.js
@@ -1,7 +1,7 @@
 export default function RadioFake(props) {
   return (
     <fieldset className="radioFake">
-      <input {...props} type={"checkbox"} className="radioFake__input" name={`customer-${props.name}`} id={`customer-order${props.id}`} checked={props.checked} />
+      <input {...props} type={"radio"} className="radioFake__input" name={`customer-${props.name}`} id={`customer-order${props.id}`} defaultChecked={props.checked} />
       <label htmlFor={`customer-order${props.id}`} className="radioFake__fake">{props.description}</label>
     </fieldset>
   )
