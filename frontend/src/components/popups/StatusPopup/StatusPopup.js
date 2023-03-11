56edfdf8e840ecdaf5027d418b6ff6c0f31e11ab
Alexandr <102781985+Eshenok@users.noreply.github.com>
2023-03-11 21:33:50 +0600

    error popup

--- frontend/src/components/popups/StatusPopup/StatusPopup.js
+++ frontend/src/components/popups/StatusPopup/StatusPopup.js
@@ -1,17 +1,23 @@
 import failureImage from '../../../images/fail.png';
 import { useDispatch, useSelector } from "react-redux";
 import { clearError } from "../../../utils/store/statusAppSlice";
+import { toggleCheckedCustomer } from "../../../utils/store/customersSlice";
 
 export function StatusPopup() {
 
   const dispatch = useDispatch();
   const {status} = useSelector(state => state.status);
 
+  function handleClose() {
+    dispatch(clearError());
+    dispatch(toggleCheckedCustomer({id: null}));
+  }
+
   return(
     <div className={`statusPopup ${status === "rejected" ? "statusPopup_opened" : ""}`}>
       <img src={failureImage} className="statusPopup__image"/>
       <p className="statusPopup__caption">{`Упс... Произошла ошибка, попробуйте еще раз`}</p>
-      <button className="button button_icon_close button_place_statusPopup" onClick={() => {dispatch(clearError())}} />
+      <button className="button button_icon_close button_place_statusPopup" onClick={handleClose} />
     </div>
   )
 }
