0784f93660c941a79ef102dec655f019b4a825c6
Захаров Павел <pzakharov83@gmail.com>
2023-04-03 12:36:14 +0300

    add logging

---
+++ backend/voucher/components/_log.py
@@ -0,0 +1,37 @@
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]',
+        },
+    },
+    'handlers': {
+        'debug-console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+            'filters': ['require_debug_true'],
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['debug-console'],
+            'propagate': False,
+        },
+        'django': {
+            'handlers': ['debug-console'],
+            'propogate': True,
+        },
+        'django.request': {
+            'handlers': ['debug-console'],
+            'level': 'ERROR',
+            'propogate': False,
+        },
+    },
+}
