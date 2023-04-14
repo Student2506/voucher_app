0dff3226978408b9d70be575468f5e11e289b814
Alexandr <102781985+Eshenok@users.noreply.github.com>
2023-04-14 20:29:27 +0600

    refatcor changeDate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,17 +18,17 @@ function App() {
   const {loggedIn, userData} = useSelector(state => state.user);
 
   useEffect(() => {
-    // if (document.cookie) {
-    //   const jwt = document.cookie.split('; ').reduce(function(result, v, i, a) { var k = v.split('='); result[k[0]] = k[1]; return result; }, {})
-    //   if (!jwt.auth_refresh) {
-    //     window.location.replace(`${baseUrl}/api/v1/oauth2/login`);
-    //   } else {
-    //     dispatch(updateJwt({jwtRefresh: jwt.auth_refresh}))
-    //   }
-    // } else {
-    //   window.location.replace(`${baseUrl}/api/v1/oauth2/login`);
-    // }
-    dispatch(updateJwt({jwtRefresh: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY4MjE2NTQ1OSwianRpIjoiMTRjZDJiNzU0N2M0NDgxYjg4Nzc5MzIxNTBmODJmNDIiLCJ1c2VyX2lkIjoiYS52b2xvc2hpbiJ9.qrcSUB3q-rbeQ3eidR4tKNrgtz05ShwOFJkCJKpm_1U"}))
+    if (document.cookie) {
+      const jwt = document.cookie.split('; ').reduce(function(result, v, i, a) { var k = v.split('='); result[k[0]] = k[1]; return result; }, {})
+      if (!jwt.auth_refresh) {
+        window.location.replace(`${baseUrl}/api/v1/oauth2/login`);
+      } else {
+        dispatch(updateJwt({jwtRefresh: jwt.auth_refresh}))
+      }
+    } else {
+      window.location.replace(`${baseUrl}/api/v1/oauth2/login`);
+    }
+    // dispatch(updateJwt({jwtRefresh: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY4MjE2NTQ1OSwianRpIjoiMTRjZDJiNzU0N2M0NDgxYjg4Nzc5MzIxNTBmODJmNDIiLCJ1c2VyX2lkIjoiYS52b2xvc2hpbiJ9.qrcSUB3q-rbeQ3eidR4tKNrgtz05ShwOFJkCJKpm_1U"}))
   }, [])
 
   useEffect(() => {
