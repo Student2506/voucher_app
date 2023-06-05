f5f588e7f511ea5214bbbeda114daaa9b0bf53a3
Захаров Павел <pzakharov83@gmail.com>
2023-06-05 21:05:01 +0300

    setup logging

--- worker_v2/html_to_pdf/settings/logging.py
+++ worker_v2/html_to_pdf/settings/logging.py
@@ -51,7 +51,7 @@ LOGGING = {
             'handlers': LOG_DEFAULT_HANDLERS,
             'level': 'DEBUG',
         },
-        'fontTools.*': {
+        'fontTools': {
             'handlers': LOG_DEFAULT_HANDLERS,
             'level': 'WARNING',
         },
