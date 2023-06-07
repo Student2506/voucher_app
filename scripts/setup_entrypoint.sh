09921d614986f849b0435b68acb38fdd5bd3b3ad
Захаров Павел <pzakharov83@gmail.com>
2023-06-07 23:27:44 +0300

    adjust elk

--- scripts/setup_entrypoint.sh
+++ scripts/setup_entrypoint.sh
@@ -1,43 +1,44 @@
 #!/bin/bash
 
+
 if [ x${ELASTIC_PASSWORD} == x ]; then
-          echo "Set the ELASTIC_PASSWORD environment variable in the .env file";
-          exit 1;
-        elif [ x${KIBANA_PASSWORD} == x ]; then
-          echo "Set the KIBANA_PASSWORD environment variable in the .env file";
-          exit 1;
-        fi;
-        if [ ! -f config/certs/ca.zip ]; then
-          echo "Creating CA";
-          bin/elasticsearch-certutil ca --silent --pem -out config/certs/ca.zip;
-          unzip config/certs/ca.zip -d config/certs;
-        fi;
-        if [ ! -f config/certs/certs.zip ]; then
-          echo "Creating certs";
-          echo -ne \
-          "instances:\n"\
-          "  - name: elasticsearch\n"\
-          "    dns:\n"\
-          "      - elasticsearch\n"\
-          "      - localhost\n"\
-          "    ip:\n"\
-          "      - 127.0.0.1\n"\
-          > config/certs/instances.yml;
-          bin/elasticsearch-certutil cert --silent --pem -out config/certs/certs.zip --in config/certs/instances.yml --ca-cert config/certs/ca/ca.crt --ca-key config/certs/ca/ca.key;
-          unzip config/certs/certs.zip -d config/certs;
-        fi;
-        echo "Setting file permissions"
-        chown -R root:root config/certs;
-        find . -type d -exec chmod 750 \{\} \;;
-        find . -type f -exec chmod 640 \{\} \;;
-        echo "Waiting for Elasticsearch availability";
-        until curl -s --cacert config/certs/ca/ca.crt https://elasticsearch:9200 | grep -q "missing authentication credentials"; do sleep 30; done;
-        echo "Setting kibana_system password";
-        until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://elasticsearch:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}"; do sleep 10; done;
-        echo "Setting logstash role";
-        until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://elasticsearch:9200/_security/role/logstash_writer -d "{\"cluster\": [\"manage_index_templates\", \"monitor\", \"manage_ilm\"], \"indices\": [{\"names\": [\"logstash-*\"], \"privileges\": [\"write\",\"create\",\"create_index\",\"manage\",\"manage_ilm\",\"auto_configure\"]}]}" | grep -q "^{\"role\":{\"created\".*}}"; do sleep 10; done;
-        echo "Setting logstash_internal user";
-        until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://elasticsearch:9200/_security/user/logstash_internal -d "{\"password\" : \"${LOGSTASH_PASSWORD}\", \"roles\" : [ \"logstash_writer\"], \"full_name\" : \"Internal Logstash User\" }" | grep -q "^{\"created\":true}"; do sleep 10; done;
-        echo "Setting logstash_system user";
-        until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://elasticsearch:9200/_security/user/logstash_system/_password -d "{\"password\" : \"${LOGSTASH_PASSWORD}\"}" | grep -q "^{}"; do sleep 10; done;
-        echo "All done!";
+    echo "Set the ELASTIC_PASSWORD environment variable in the .env file";
+    exit 1;
+elif [ x${KIBANA_PASSWORD} == x ]; then
+    echo "Set the KIBANA_PASSWORD environment variable in the .env file";
+    exit 1;
+fi;
+if [ ! -f config/certs/ca.zip ]; then
+    echo "Creating CA";
+    bin/elasticsearch-certutil ca --silent --pem -out config/certs/ca.zip;
+    unzip config/certs/ca.zip -d config/certs;
+fi;
+if [ ! -f config/certs/certs.zip ]; then
+    echo "Creating certs";
+    echo -ne \
+    "instances:\n"\
+    "  - name: elasticsearch\n"\
+    "    dns:\n"\
+    "      - elasticsearch\n"\
+    "      - localhost\n"\
+    "    ip:\n"\
+    "      - 127.0.0.1\n"\
+    > config/certs/instances.yml;
+    bin/elasticsearch-certutil cert --silent --pem -out config/certs/certs.zip --in config/certs/instances.yml --ca-cert config/certs/ca/ca.crt --ca-key config/certs/ca/ca.key;
+    unzip config/certs/certs.zip -d config/certs;
+fi;
+echo "Setting file permissions"
+chown -R root:root config/certs;
+find . -type d -exec chmod 750 \{\} \;;
+find . -type f -exec chmod 640 \{\} \;;
+echo "Waiting for Elasticsearch availabiliy";
+until curl -s --cacert config/certs/ca/ca.crt https://elasticsearch:9200 | grep -q "missing authentication credentials"; do sleep 30; done;
+echo "Setting kibana_system password";
+until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://elasticsearch:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}"; do sleep 10; done;
+echo "Setting logstash role";
+until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://elasticsearch:9200/_security/role/logstash_writer -d "{\"cluster\": [\"manage_index_templates\", \"monitor\", \"manage_ilm\"], \"indices\": [{\"names\": [\"*\"], \"privileges\": [\"write\",\"create\",\"create_index\",\"manage\",\"manage_ilm\",\"auto_configure\"]}]}" | grep -q "^{\"role\":{\"created\".*}}"; do sleep 10; done;
+echo "Setting logstash_internal user";
+until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://elasticsearch:9200/_security/user/logstash_internal -d "{\"password\" : \"${LOGSTASH_PASSWORD}\", \"roles\" : [ \"logstash_writer\"], \"full_name\" : \"Internal Logstash User\" }" | grep -q "^{\"created\":true}"; do sleep 10; done;
+echo "Setting logstash_system user";
+until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://elasticsearch:9200/_security/user/logstash_system/_password -d "{\"password\" : \"${LOGSTASH_PASSWORD}\"}" | grep -q "^{}"; do sleep 10; done;
+echo "All done!";
