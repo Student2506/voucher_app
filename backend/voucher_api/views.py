656b08eefdd35737b71d8b208c39d3433bb8d640
Захаров Павел <pzakharov83@gmail.com>
2023-10-12 17:25:54 +0300

    add redeemed

--- backend/voucher_api/views.py
+++ backend/voucher_api/views.py
@@ -276,7 +276,7 @@ class UpdateExpiry(views.APIView):
         logger.info('============')
         for expired_card in expired_cards:
             reedem = RedeemedCard.objects.get(
-                voucher_type_id=expired_card.voucher_type_id.pk,
+                voucher_type_id=expired_card.voucher_type_id,
                 voucher_number=expired_card.voucher_number,
                 duplicate_no=expired_card.duplicate_no,
             )
