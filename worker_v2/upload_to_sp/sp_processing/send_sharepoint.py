617add7553611a8c563851a7263eb50948591178
Захаров Павел <pzakharov83@gmail.com>
2023-12-08 16:00:04 +0300

    fix sharepoint

--- worker_v2/upload_to_sp/sp_processing/send_sharepoint.py
+++ worker_v2/upload_to_sp/sp_processing/send_sharepoint.py
@@ -13,16 +13,21 @@ logger = get_logger(__name__)
 
 
 class SharePoint:
-
     def __init__(self) -> None:
-        self.conn = ClientContext(settings.sharepoint_site).with_credentials(
+        self.conn = ClientContext(str(settings.sharepoint_site)).with_credentials(
             ClientCredential(settings.sharepoint_client_id, settings.sharepoint_secret),
         )
 
     def create_folder(self, folder_name: str) -> Any:
         """Create Folder at Exchange Folder."""
-        target_folder_url_path = Path('/sites') / settings.sharepoint_site_name / settings.sharepoint_doc_library
-        root_folder = self.conn.web.get_folder_by_server_relative_url(target_folder_url_path.as_posix())
+        target_folder_url_path = (
+            Path('/sites')
+            / settings.sharepoint_site_name
+            / settings.sharepoint_doc_library
+        )
+        root_folder = self.conn.web.get_folder_by_server_relative_url(
+            target_folder_url_path.as_posix()
+        )
         target_folder = root_folder.folders.add(folder_name).execute_query()
         logger.debug(f'Created {target_folder}')
         return target_folder
@@ -30,11 +35,18 @@ class SharePoint:
     def upload_file(self, file_name: str, folder_name: str, content: Any) -> str:
         """Upload file to specific folder."""
         target_folder_url_path = (
-            Path('/sites') / settings.sharepoint_site_name / settings.sharepoint_doc_library / folder_name
+            Path('/sites')
+            / settings.sharepoint_site_name
+            / settings.sharepoint_doc_library
+            / folder_name
         )
-        target_folder = self.conn.web.get_folder_by_server_relative_path(target_folder_url_path.as_posix())
+        target_folder = self.conn.web.get_folder_by_server_relative_path(
+            target_folder_url_path.as_posix()
+        )
         # target_folder.upload_file(file_name, content).execute_query()
-        target_folder.files.create_upload_session(content, chunk_size=100_000_000).execute_query()
+        target_folder.files.create_upload_session(
+            content, chunk_size=100_000_000
+        ).execute_query()
         return f'{settings.sharepoint_site}/{settings.sharepoint_doc_library}/{folder_name}/{file_name}'
 
     def get_share_link(self, file_path: str) -> Any:
