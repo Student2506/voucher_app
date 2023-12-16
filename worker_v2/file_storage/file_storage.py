eeb6793c4adf5947e4b942f600d2c0ca8342b2c4
Захаров Павел <pzakharov83@gmail.com>
2023-12-16 21:45:38 +0300

    add file access

--- worker_v2/file_storage/file_storage.py
+++ worker_v2/file_storage/file_storage.py
@@ -1,3 +1,4 @@
+import aiofiles
 from aiohttp import web
 
 from settings.config import get_logger, settings
@@ -13,6 +14,11 @@ async def get_file(request: web.Request) -> web.Response:
     file_name = request.match_info.get('file_name')
     logger.debug(f'Getting file: {file_hash}\n{file_name}')
     result = await request.app['redis'].exists(file_hash)
+    if result == 1:
+        filename = request.app['redis'].get(file_hash)
+        filename = Path('storage') / filename
+        logger.debug(filename)
+        # async with aiofiles.open()
     logger.debug(result)
     return web.Response(text='Hi Anya!')
 
