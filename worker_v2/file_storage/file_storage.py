1fe22d769ead37a32081893d9e4ec2ea626c5b12
Захаров Павел <pzakharov83@gmail.com>
2023-12-16 18:08:42 +0300

    add stub

--- worker_v2/file_storage/file_storage.py
+++ worker_v2/file_storage/file_storage.py
@@ -19,7 +19,11 @@ async def hello(request: web.Request) -> web.Response:
     return web.Response(text='Hello, Aiohttp!')
 
 
-if __name__ == '__main__':
+def main() -> None:
     app = web.Application()
     app.add_routes(routes)
     web.run_app(app, port=settings.port_to_listen)
+
+
+if __name__ == '__main__':
+    main()
