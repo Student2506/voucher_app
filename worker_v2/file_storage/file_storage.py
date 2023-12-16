7343cbf0203f713499a4b36b709137f11c997191
Захаров Павел <pzakharov83@gmail.com>
2023-12-16 17:41:11 +0300

    add stub

--- worker_v2/file_storage/file_storage.py
+++ worker_v2/file_storage/file_storage.py
@@ -2,27 +2,18 @@ from aiohttp import web
 
 from settings.config import get_logger, settings
 
-routes = web.RouteTableDef()
 logger = get_logger(__name__)
 
 
-@routes.get('/files/{file_hash}/{file_name}')
-async def get_file(request: web.Request) -> web.Response:
-    file_hash = request.match_info.get('file_hash')
-    file_name = request.match_info.get('file_name')
-    logger.debug(f'Getting file: {file_hash}\n{file_name}')
-    return web.Response(text='Hi Anya!', content_type='text/html')
+routes = web.RouteTableDef()
 
 
-async def index(request: web.Request) -> web.Response:
-    return web.Response(text='Hello Aiohttp!')
+@routes.get('/')
+async def hello(request: web.Request) -> web.Response:
+    return web.Response(text='Hello, World')
 
 
-def setup_routes(app: web.Application) -> None:
-    app.router.add_get('/', index)
-
-
-if __name__ == '__main__':
-    app = web.Application()
-    setup_routes(app)
-    web.run_app(app, port=settings.port_to_listen)
+app = web.Application()
+app.add_routes(routes=routes)
+web.run_app
+print(settings.port_to_listen)
