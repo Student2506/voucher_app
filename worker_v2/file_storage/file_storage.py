4d5853f5ffe1c3292e91330e59d3ca5cf02d1523
Захаров Павел <pzakharov83@gmail.com>
2023-12-17 12:09:10 +0300

    start over

--- worker_v2/file_storage/file_storage.py
+++ worker_v2/file_storage/file_storage.py
@@ -16,50 +16,15 @@ routes = web.RouteTableDef()
 async def get_file(request: web.Request) -> web.Response:
     file_hash = request.match_info.get('file_hash')
     file_name = request.match_info.get('file_name')
-    logger.debug(f'Getting file: {file_hash}\n{file_name}')
-    result = await request.app['redis'].exists(file_hash)
-    if result != 1:
+    is_file_exists = await request.app['redis'].exists(file_hash)
+    if is_file_exists != 1:
         raise web.HTTPNotFound()
-
     filename = await request.app['redis'].get(file_hash)
-    logger.debug(filename)
     filename = Path('storage') / filename
     file_size = filename.stat().st_size
     logger.debug(
         f'File {filename} exists: {filename.exists()}\nand has stats {file_size}'
     )
-    response = web.StreamResponse()
-    filename_encoded = quote_plus(file_name.encode("utf-8"))
-    logger.debug(filename_encoded)
-    response.headers[
-        'Content-Disposition'
-    ] = f'attachment; filename={filename_encoded}.zip'
-    response.headers['Content-Length'] = str(file_size)
-    response.headers['Transfer-Encoding'] = 'deflate; chunked'
-    response.headers['Connection'] = 'keep-alive'
-    response.headers['Content-Type'] = 'text/html'
-    response.headers
-    await response.prepare(request)
-    logger.debug(response)
-
-    try:
-        async with aiofiles.open(filename, 'rb') as fh:
-            while True:
-                next_piece = bytearray(await fh.read(settings.chunk_size))
-                await asyncio.sleep(1)
-                if not next_piece:
-                    break
-                await response.write(next_piece)
-                logger.debug('Sending next chunck...')
-    except Exception as e:
-        logger.error(str(e))
-    except BaseException as e:
-        logger.error(str(e))
-    finally:
-        logger.debug('Done transfer')
-        await response.write_eof()
-
-    # return web.Response(text='Hi Anya!')
 
 
 @routes.get('/')
