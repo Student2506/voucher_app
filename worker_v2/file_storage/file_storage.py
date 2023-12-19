10778bad6276302b428de0561881d3ebb7e1d51c
Захаров Павел <pzakharov83@gmail.com>
2023-12-19 21:43:26 +0300

    add logging

--- worker_v2/file_storage/file_storage.py
+++ worker_v2/file_storage/file_storage.py
@@ -57,7 +57,7 @@ async def get_file(request: web.Request) -> web.StreamResponse():
     response = web.StreamResponse()
     filename_encoded = quote_plus(file_name.encode("utf-8"))
     response.headers['Content-Disposition'] = f'attachment; filename={filename_encoded}'
-    response.headers['Transfer-Encoding'] = 'deflate; chunked'
+    response.headers['Transfer-Encoding'] = 'chunked'
     response.headers['Connection'] = 'keep-alive'
     response.headers['Content-Type'] = 'text/html'
     await response.prepare(request)
